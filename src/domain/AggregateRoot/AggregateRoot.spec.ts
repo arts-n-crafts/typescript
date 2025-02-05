f41be109d952250be2e3c61f2be4aff55313eb74
Jens van Hellemondt <jens@invictus.codes>
2025-02-05 10:01:31 +0100

    feat(AggregateRoot): should be defined

---
+++ src/domain/AggregateRoot/AggregateRoot.spec.ts
@@ -0,0 +1,8 @@
+import { describe, it, expect } from "vitest";
+import { AggregateRoot } from "./AggregateRoot";
+
+describe("AggregateRoot", () => {
+  it("should be defined", () => {
+    expect(AggregateRoot).toBeDefined();
+  });
+});
--- src/domain/AggregateRoot/AggregateRoot.ts
+++ src/domain/AggregateRoot/AggregateRoot.ts
@@ -1,8 +1,24 @@
 import type { IDomainEvent } from "../DomainEvent/DomainEvent";
-import type { IEntity } from "../Entity/Entity";
+import { Entity, type IEntity } from "../Entity/Entity";
 
 export interface IAggregateRoot extends IEntity {
   apply(event: IDomainEvent): void;
   getUncommittedEvents(): IDomainEvent[];
   markEventsCommitted(): void;
+}
+
+export abstract class AggregateRoot<AggregateRootProps extends object>
+  extends Entity<AggregateRootProps>
+  implements IAggregateRoot
+{
+  apply(_event: IDomainEvent): void {
+    throw new Error("Method not implemented.");
+  }
+  getUncommittedEvents(): IDomainEvent[] {
+    throw new Error("Method not implemented.");
+  }
+  markEventsCommitted(): void {
+    throw new Error("Method not implemented.");
+  }
+
 }
