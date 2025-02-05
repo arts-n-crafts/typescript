953f6bdc5efcf5086e265d5a02542dd087e0d74f
Jens van Hellemondt <jens@invictus.codes>
2025-02-05 10:04:08 +0100

    feat(Entity): apply factory pattern

--- src/domain/Entity/Entity.ts
+++ src/domain/Entity/Entity.ts
@@ -1,7 +1,7 @@
 import type { Maybe } from "../../core/types/Maybe";
 
 export interface IEntity {
-  readonly id: Maybe<string>;                     // The unique identifier for the entity
+  readonly id: Maybe<string>;                   // The unique identifier for the entity
   readonly props: object;                       // The properties of the entity
   equals(other: IEntity): boolean;              // Equality based on ID
 }
@@ -10,7 +10,7 @@ export abstract class Entity<TProps extends IEntity['props']> implements IEntity
   readonly _id: IEntity['id'];
   readonly _props: TProps;
 
-  constructor(props: TProps, id: IEntity['id']) {
+  protected constructor(props: TProps, id: IEntity['id']) {
     this._id = id;
     this._props = props;
   }
--- src/domain/Entity/mocks/MockUser.ts
+++ src/domain/Entity/mocks/MockUser.ts
@@ -5,4 +5,8 @@ interface MockUserProps {
   email: string;
 }
 
-export class MockUser extends Entity<MockUserProps> { }
+export class MockUser extends Entity<MockUserProps> {
+  static create(props: MockUserProps, id: string) {
+    return new MockUser(props, id);
+  }
+}
