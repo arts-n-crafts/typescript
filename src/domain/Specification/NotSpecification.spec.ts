e78c1426b812166f4f01c20d586de0cf576c64b2
Jens van Hellemondt <jens@invictus.codes>
2025-02-05 15:49:51 +0100

    feat(Specificaton): NotSpecification should be defined

---
+++ src/domain/Specification/NotSpecification.spec.ts
@@ -0,0 +1,8 @@
+import { describe, it, expect } from "vitest";
+import { NotSpecification } from "./NotSpecification";
+
+describe('NotSpecification', () => {
+  it('should be defined', () => {
+    expect(NotSpecification).toBeDefined();
+  });
+});
