15b9c612a9553797fd152c227dad3b3c333ba456
Jens van Hellemondt <jens@invictus.codes>
2025-02-07 15:14:26 +0100

    feat(EventHandler): should be defined

---
+++ src/infrastructure/EventBus/EventHandler.spec.ts
@@ -0,0 +1,8 @@
+import { describe, it, expect, beforeEach } from "vitest";
+import { EventHandler } from "./EventHandler";
+
+describe('EventHandler', () => {
+  it('should be defined', () => {
+    expect(EventHandler).toBeDefined();
+  });
+});
--- src/infrastructure/EventBus/EventHandler.ts
+++ src/infrastructure/EventBus/EventHandler.ts
@@ -1,5 +1,5 @@
 import { DomainEvent } from "../../domain/DomainEvent/DomainEvent";
 
-export interface EventHandler<TEvent extends DomainEvent<unknown>> {
-  handle(event: TEvent): Promise<void>;
+export abstract class EventHandler<TEvent extends DomainEvent<unknown>> {
+  abstract handle(event: TEvent): Promise<void>;
 }
