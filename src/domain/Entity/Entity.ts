1bc31f2817900695b03bcaa28c5d33918c684973
Jens van Hellemondt <jens@invictus.codes>
2025-02-07 14:42:24 +0100

    refactor(Entity): simplify types

--- src/domain/Entity/Entity.ts
+++ src/domain/Entity/Entity.ts
@@ -1,21 +1,14 @@
-import type { Maybe } from "../../core/types/Maybe";
 
-export interface IEntity<TProps> {
-  readonly id: Maybe<string>;                   // The unique identifier for the entity
-  readonly props: TProps;                       // The properties of the entity
-  equals(other: IEntity<TProps>): boolean;              // Equality based on ID
-}
-
-export abstract class Entity<TProps> implements IEntity<TProps> {
-  readonly _id: IEntity<TProps>['id'];
+export abstract class Entity<TProps> {
+  readonly _id: string;
   readonly _props: TProps;
 
-  protected constructor(props: TProps, id: IEntity<TProps>['id']) {
+  protected constructor(props: TProps, id: string) {
     this._id = id;
     this._props = props;
   }
 
-  get id(): IEntity<TProps>['id'] {
+  get id(): string {
     return this._id;
   }
 
@@ -23,7 +16,7 @@ export abstract class Entity<TProps> implements IEntity<TProps> {
     return this._props;
   }
 
-  equals(other: IEntity<TProps>): boolean {
+  equals(other: Entity<unknown>): boolean {
     return this.id === other.id;
   }
 }
