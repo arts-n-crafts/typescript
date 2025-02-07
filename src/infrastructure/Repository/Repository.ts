a8f488eda8613f09b1bc9dfc7c7acfff7278ec09
Jens van Hellemondt <jens@invictus.codes>
2025-02-07 14:40:05 +0100

    refactor(Repository): simplify types

--- src/infrastructure/Repository/Repository.ts
+++ src/infrastructure/Repository/Repository.ts
@@ -1,13 +1,7 @@
 import type { IAggregateRoot } from "../../domain/AggregateRoot/AggregateRoot";
 import type { EventStore } from "../EventStore/EventStore";
 
-interface IRepository<TAggregate extends IAggregateRoot<TAggregate['props']>> {
-  load(aggregateId: string): Promise<IAggregateRoot<TAggregate['props']>>;
-  store(aggregate: IAggregateRoot<TAggregate['props']>): Promise<void>;
-}
-
 export abstract class Repository<TAggregate extends IAggregateRoot<TAggregate['props']>>
-implements IRepository<TAggregate>
 {
   constructor(
     protected readonly eventStore: EventStore
