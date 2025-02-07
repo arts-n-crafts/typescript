23bfe5c05a173e752281a6a22eee9952d0b59f04
Jens van Hellemondt <jens@invictus.codes>
2025-02-07 14:48:12 +0100

    refactor(ScenarioTest): simplify types

--- src/infrastructure/ScenarioTest/ScenarioTest.ts
+++ src/infrastructure/ScenarioTest/ScenarioTest.ts
@@ -3,13 +3,13 @@ import type { Command } from "../CommandBus/Command";
 import type { Query } from "../QueryBus/Query";
 
 interface _CommandScenarioTest {
-  given: (events: DomainEvent[]) => void;
-  when: (command: Command) => void;
-  then: (events: DomainEvent[]) => void;
+  given: (events: DomainEvent<unknown>[]) => void;
+  when: (command: Command<unknown>) => void;
+  then: (events: DomainEvent<unknown>[]) => void;
 }
 
 interface _QueryScenarioTest {
-  given: (events: DomainEvent[]) => void;
-  when: (query: Query) => void;
-  then: (result: object) => void;
+  given: (events: DomainEvent<unknown>[]) => void;
+  when: (query: Query<unknown>) => void;
+  then: (result: unknown) => void;
 }
