0ddac9db5fd9a470eec824766fb6f8583b8ff541
Jens van Hellemondt <jens@invictus.codes>
2025-02-07 11:20:42 +0100

    feat(Repository): should rehydrate the aggregate based on its events

--- src/infrastructure/Repository/Repository.spec.ts
+++ src/infrastructure/Repository/Repository.spec.ts
@@ -8,31 +8,33 @@ import { MockUser } from "../../domain/AggregateRoot/mocks/MockUser";
 describe('Repository', () => {
   let aggregateId: string;
   let eventStore: EventStore;
-  let event: MockUserNameUpdatedEvent;
+  let mockUserNameUpdateEvent: MockUserNameUpdatedEvent;
   let aggregateRoot: MockUser;
 
   beforeEach(() => {
     aggregateId = '123';
     eventStore = new InMemoryEventStore();
-    event = new MockUserNameUpdatedEvent( '123', {name: 'musk'}, );
+    mockUserNameUpdateEvent = new MockUserNameUpdatedEvent( '123', { name: 'musk' }, );
     aggregateRoot = MockUser.create({ name: 'elon', email: 'elon@x.com', }, aggregateId);
-    aggregateRoot.apply(event);
+    aggregateRoot.apply(mockUserNameUpdateEvent);
   });
 
   it('should be defined', () => {
     expect(MockRepository).toBeDefined();
-  })
-
-  it('should be able to load all events of an aggregate', () => {
-    const repository = new MockRepository(eventStore);
-    expect(repository.load).toBeDefined();
   });
 
   it('should be able to store a new event from an aggregate', async () => {
     const repository = new MockRepository(eventStore);
     repository.store(aggregateRoot);
     const events = await eventStore.loadEvents(aggregateId)
-    expect(events[1]).toStrictEqual(event);
+    expect(events[1]).toStrictEqual(mockUserNameUpdateEvent);
     expect(events).toHaveLength(2);
+  });
+
+  it('should rehydrate the aggregate based on it\'s events', async () => {
+    const repository = new MockRepository(eventStore);
+    repository.store(aggregateRoot);
+    const aggregate = await repository.load(aggregateId);
+    expect(aggregate.props.name).toBe(mockUserNameUpdateEvent.payload.name);
   });
 });
--- src/infrastructure/Repository/mocks/MockRepository.ts
+++ src/infrastructure/Repository/mocks/MockRepository.ts
@@ -1,9 +1,12 @@
 import type { IAggregateRoot } from "../../../domain/AggregateRoot/AggregateRoot";
+import { MockUser } from "../../../domain/AggregateRoot/mocks/MockUser";
 import { Repository } from "../Repository";
 
 export class MockRepository extends Repository {
-  load(_aggregateId: string): Promise<IAggregateRoot> {
-    throw new Error("Method not implemented.");
+  async load(aggregateId: string): Promise<IAggregateRoot> {
+    const events = await this.eventStore.loadEvents(aggregateId);
+    const aggregate = MockUser.rehydrate(aggregateId, events);
+    return aggregate
   }
 
   async store(aggregate: IAggregateRoot): Promise<void> {
