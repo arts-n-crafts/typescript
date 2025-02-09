3f34fc5d2ea4b9aa20a57f0fa340d57071b10954
Jens van Hellemondt <jens@invictus.codes>
2025-02-09 17:08:49 +0100

    feat(ProjectionHandler): should be defined

---
+++ src/infrastructure/ProjectionHandler/ProjectionHandler.spec.ts
@@ -0,0 +1,8 @@
+import { describe, it, expect } from "vitest";
+import { ProjectionHandler } from "./ProjectionHandler";
+
+describe("ProjectionHandler", () => {
+  it("should be defined", () => {
+    expect(ProjectionHandler).toBeDefined();
+  });
+});
--- src/infrastructure/ProjectionHandler/ProjectionHandler.ts
+++ src/infrastructure/ProjectionHandler/ProjectionHandler.ts
@@ -1,6 +1,8 @@
 import { DomainEvent } from "../../domain/DomainEvent/DomainEvent";
 
-export interface ProjectionHandler {
+export interface IProjectionHandler {
   start(): void;
   updateProjection(event: DomainEvent<unknown>): Promise<void>;
 }
+
+export abstract class ProjectionHandler { }
