6ec2b4a78ccd9baa0162eec9bdec0cce90e9068b
Jens van Hellemondt <jens@invictus.codes>
2025-02-15 10:11:54 +0100

    feat(Specification): should be defined

--- src/domain/Specification/Specification.spec.ts
+++ src/domain/Specification/Specification.spec.ts
@@ -1,8 +1,13 @@
 import { describe, it, expect } from "vitest";
 import { MockUserByUsernameSpecification } from "./mocks/MockUserByUsernameSpecification";
 import { MockUser } from "../AggregateRoot/mocks/MockUser";
+import { Specification } from "./Specification";
 
 describe('Base Specification', () => {
+  it('should be defined', () => {
+    expect(Specification).toBeDefined();
+  });
+
   it('should be satisfied', () => {
     const candidate = 'John Doe'
     const specification = new MockUserByUsernameSpecification(candidate);
