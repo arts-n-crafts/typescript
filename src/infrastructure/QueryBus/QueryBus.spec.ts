c8ec1ef35a0f616d4893f0150ccd9b62a63bd184
Jens van Hellemondt <jens@invictus.codes>
2025-02-17 19:51:50 +0100

    feat(QueryBus): should throw an error if the query handler is not registered

--- src/infrastructure/QueryBus/QueryBus.spec.ts
+++ src/infrastructure/QueryBus/QueryBus.spec.ts
@@ -1,9 +1,23 @@
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, beforeEach } from "vitest";
 import { QueryBus } from "./QueryBus";
+import { MockGetUserByEmailQuery, type MockGetUserByEmailQueryProps } from "./mocks/MockGetUserByEmailQuery";
 
 describe("QueryBus", () => {
+  let payload: MockGetUserByEmailQueryProps;
+
+  beforeEach(() => {
+    payload = { email: 'test' }
+  })
+
   it('should be defined', () => {
     const bus = new QueryBus();
     expect(bus).toBeDefined();
+  });
+
+  it('should throw an error if the query handler is not registered', async () => {
+    const query = new MockGetUserByEmailQuery(payload)
+    const bus = new QueryBus();
+    const promised = bus.execute(query)
+    await expect(promised).rejects.toThrowError(`No handler found for query type: ${MockGetUserByEmailQuery.name}`);
   });
 });
--- src/infrastructure/QueryBus/QueryBus.ts
+++ src/infrastructure/QueryBus/QueryBus.ts
@@ -8,17 +8,24 @@ export interface IQueryBus {
     query: new (payload: TPayload, metadata: Maybe<CommandMetadata>) => Query<TPayload>,
     handler: QueryHandler<Query<TPayload>, TResult>
   ): void;
-  execute<TQuery, TResult>(query: TQuery): Promise<TResult>;
+  execute(query: Query<unknown>): Promise<unknown>;
 }
 
 export class QueryBus implements IQueryBus {
+  private handlers: Map<string, QueryHandler<Query<unknown>, unknown>> = new Map();
+
   register<TPayload, TResult>(
-    _query: new (payload: TPayload, metadata: Maybe<CommandMetadata>) => Query<TPayload>, 
+    _query: new (payload: TPayload, metadata: Maybe<CommandMetadata>) => Query<TPayload>,
     _handler: QueryHandler<Query<TPayload>, TResult>
   ): void {
     throw new Error("Method not implemented.");
   }
-  execute<TQuery, TResult>(_query: TQuery): Promise<TResult> {
+
+  async execute(query: Query<unknown>): Promise<unknown> {
+    const handler = this.handlers.get(query.constructor.name)
+    if (!handler) {
+      throw new Error(`No handler found for query type: ${query.constructor.name}`);
+    }
     throw new Error("Method not implemented.");
   }
 }
