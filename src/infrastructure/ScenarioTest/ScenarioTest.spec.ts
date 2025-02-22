7c30c329c0f1aa309499d637bf96a59ada79041f
Jens van Hellemondt <jens@invictus.codes>
2025-02-22 22:44:24 +0100

    feat(ScenarioTest): should add the events provided to the given step to the eventStore

--- src/infrastructure/ScenarioTest/ScenarioTest.spec.ts
+++ src/infrastructure/ScenarioTest/ScenarioTest.spec.ts
@@ -1,8 +1,37 @@
-import { describe, expect, it } from 'vitest'
+import type { EventStore } from '../EventStore/EventStore'
+import { randomUUID } from 'node:crypto'
+import { beforeEach, describe, expect, it } from 'vitest'
+import { MockUserCreatedEvent } from '../../domain/DomainEvent/mocks/MockUserCreated'
+import { CommandBus } from '../CommandBus/CommandBus'
+import { EventBus } from '../EventBus/EventBus'
+import { InMemoryEventStore } from '../EventStore/implementations/InMemoryEventStore'
+import { QueryBus } from '../QueryBus/QueryBus'
 import { ScenarioTest } from './ScenarioTest'
 
 describe('scenario test', () => {
+  let eventStore: EventStore
+  let eventBus: EventBus
+  let commandBus: CommandBus
+  let queryBus: QueryBus
+  let scenarioTest: ScenarioTest
+
+  beforeEach(() => {
+    eventBus = new EventBus()
+    eventStore = new InMemoryEventStore(eventBus)
+    commandBus = new CommandBus()
+    queryBus = new QueryBus()
+    scenarioTest = new ScenarioTest(eventStore, eventBus, commandBus, queryBus)
+  })
+
   it('should be defined', async () => {
     expect(ScenarioTest).toBeDefined()
+  })
+
+  it('should add the events provided to the given step to the eventStore', async () => {
+    const id = randomUUID()
+    await scenarioTest.given(
+      new MockUserCreatedEvent(id, { name: 'Elon', email: 'musk@theboringcompany.com' }),
+    )
+    expect(await eventStore.loadEvents(id)).toHaveLength(1)
   })
 })
--- src/infrastructure/ScenarioTest/ScenarioTest.ts
+++ src/infrastructure/ScenarioTest/ScenarioTest.ts
@@ -1,10 +1,8 @@
 import type { DomainEvent } from '../../domain/DomainEvent/DomainEvent'
-import type { Command } from '../CommandBus/Command'
 import type { CommandBus } from '../CommandBus/CommandBus'
 import type { EventBus } from '../EventBus/EventBus'
 import type { EventStore } from '../EventStore/EventStore'
 import type { QueryBus } from '../QueryBus/QueryBus'
-import { expect } from 'vitest'
 
 export class ScenarioTest {
   constructor(
@@ -14,18 +12,20 @@ export class ScenarioTest {
     private readonly _queryBus: QueryBus,
   ) {}
 
-  async given(events: DomainEvent<unknown>[]): Promise<this> {
-    await Promise.all(events.map(async event => this.eventStore.store(event)))
+  async given(...events: DomainEvent<unknown>[]): Promise<this> {
+    for (const event of events) {
+      await this.eventStore.store(event)
+    }
     return this
   }
 
-  async when(command: Command<unknown>): Promise<this> {
-    await this.commandBus.execute(command)
-    return this
-  }
+  // async when(command: Command<unknown>): Promise<this> {
+  //   await this.commandBus.execute(command)
+  //   return this
+  // }
 
-  async then(event: DomainEvent<unknown>): Promise<void> {
-    const actualEvents = await this.eventStore.loadEvents(event.aggregateId)
-    expect(actualEvents[-1]).toEqual(event)
-  }
+  // async then(event: DomainEvent<unknown>): Promise<void> {
+  //   const actualEvents = await this.eventStore.loadEvents(event.aggregateId)
+  //   expect(actualEvents[-1]).toEqual(event)
+  // }
 }
