8ed462d721100191ba282242dd64f378a855260c
Jens van Hellemondt <jens@invictus.codes>
2025-04-04 00:31:44 +0200

    chore: remove unused Notifier

--- src/infrastructure/Notifier/Notifier.ts
+++
@@ -1,7 +0,0 @@
-import type { DomainEvent } from '../../domain/DomainEvent/DomainEvent'
-
-type NotificationChannel = 'websocket' | 'email' | 'sms' | 'push'
-
-export interface Notifier {
-  notify: (event: DomainEvent<unknown>, channels: NotificationChannel[]) => void
-}
