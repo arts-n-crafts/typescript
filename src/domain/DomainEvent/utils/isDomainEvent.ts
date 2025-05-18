b23793dcad7b8359c39f68feca9ebb18f7e4bc48
Jens van Hellemondt <jens@invictus.codes>
2025-05-18 10:16:39 +0200

    feat(DomainEvent): isDomainEvent allows unknown events

--- src/domain/DomainEvent/utils/isDomainEvent.ts
+++ src/domain/DomainEvent/utils/isDomainEvent.ts
@@ -1,8 +1,7 @@
-import type { BaseEvent } from '../../../infrastructure/EventBus/Event'
 import type { DomainEvent } from '../DomainEvent'
 import { isEvent } from '../../../infrastructure/EventBus/utils/isEvent'
 
-export function isDomainEvent<T = unknown>(event: BaseEvent<T>): event is DomainEvent<T> {
+export function isDomainEvent<T = unknown>(event: unknown): event is DomainEvent<T> {
   return isEvent(event)
     && event.metadata.source === 'internal'
 }
