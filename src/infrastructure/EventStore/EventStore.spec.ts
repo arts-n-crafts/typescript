b21041dd69fc0bad8827e192d1d48a0b964a49b2
jvhellemondt <j62676@eon.com>
2025-07-09 10:24:13 +0200

    feat(EventStore): should acknowledge dispatch in the outbox

--- src/infrastructure/EventStore/EventStore.spec.ts
+++ src/infrastructure/EventStore/EventStore.spec.ts
@@ -1,19 +1,16 @@
 import type { UserEvent } from '@domain/examples/User.js'
-import type { AllEvents } from '@infrastructure/ScenarioTest/examples/User.module.ts'
-import type { EventBus } from '../EventBus/EventBus.ts'
 import { randomUUID } from 'node:crypto'
 import { UserCreated } from '@domain/examples/UserCreated.ts'
 import { UserNameUpdated } from '@domain/examples/UserNameUpdated.ts'
-import { describe } from 'vitest'
-import { InMemoryEventBus } from '../EventBus/implementations/InMemoryEventBus.ts'
+import { beforeEach, describe } from 'vitest'
 import { InMemoryEventStore } from './implementations/InMemoryEventStore.ts'
 
+const makeUserStreamId = (aggregateId: string) => `user-${aggregateId}`
+
 describe('inMemoryEventStore', () => {
-  let eventBus: EventBus<AllEvents>
   let eventStore: InMemoryEventStore
 
   beforeEach(() => {
-    eventBus = new InMemoryEventBus()
     eventStore = new InMemoryEventStore()
   })
 
@@ -29,8 +26,6 @@ describe('inMemoryEventStore', () => {
   })
 
   it('should store and load multiple events', async () => {
-    const makeUserStreamId = (aggregateId: string) => `user-${aggregateId}`
-
     const aggregateId = randomUUID()
     const streamId = makeUserStreamId(aggregateId)
     const event = UserCreated(aggregateId, { name: 'elon', email: 'musk@x.com' })
@@ -56,8 +51,6 @@ describe('inMemoryEventStore', () => {
 
   describe('outbox pattern', () => {
     it('should only have unpublished entries in the outbox', async () => {
-      const makeUserStreamId = (aggregateId: string) => `user-${aggregateId}`
-
       const aggregateId = randomUUID()
       const streamId = makeUserStreamId(aggregateId)
       const event = UserCreated(aggregateId, { name: 'elon', email: 'musk@x.com' })
@@ -73,6 +66,25 @@ describe('inMemoryEventStore', () => {
 
       expect(outbox).toHaveLength(3)
       expect(outbox.every(entry => !entry.published))
+    })
+
+    it('should acknowledge dispatch in the outbox', async () => {
+      const aggregateId = randomUUID()
+      const streamId = makeUserStreamId(aggregateId)
+      const event = UserCreated(aggregateId, { name: 'elon', email: 'musk@x.com' })
+      const event2 = UserNameUpdated(aggregateId, { name: 'Donald' })
+
+      const aggregateId2 = randomUUID()
+      const streamId2 = makeUserStreamId(aggregateId2)
+      const event3 = UserCreated(aggregateId2, { name: 'Donald', email: 'potus@x.com' })
+
+      await eventStore.append(streamId, [event, event2])
+      await eventStore.append(streamId2, [event3])
+
+      eventStore.acknowledgeDispatch(event3.id)
+      const outbox = eventStore.getOutboxBatch()
+
+      expect(outbox).toHaveLength(2)
     })
   })
 })
