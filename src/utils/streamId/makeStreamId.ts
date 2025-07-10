dd9e4b1d582bf69600b89268143e25d602f4a7ba
jvhellemondt <j62676@eon.com>
2025-07-10 15:11:20 +0200

    feat: add StreamId type

--- src/utils/streamId/makeStreamId.ts
+++ src/utils/streamId/makeStreamId.ts
@@ -1,3 +1,5 @@
-export function makeStreamId<Stream extends string, Id extends string>(stream: Stream, id: Id): `${Stream}-${Id}` {
+export type StreamId = `${string}-${string}`
+
+export function makeStreamId<Stream extends string, Id extends string>(stream: Stream, id: Id): StreamId {
   return `${stream}-${id}`
 }
