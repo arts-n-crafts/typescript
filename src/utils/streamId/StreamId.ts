b8432a5fe6b982c41da4a9a5523a2d6f8b6459af
jvhellemondt <j62676@eon.com>
2025-07-11 09:04:17 +0200

    refactor(StreamId): move StreamId to separate file

---
+++ src/utils/streamId/StreamId.ts
@@ -0,0 +1,1 @@
+export type StreamId = `${string}-${string}`
--- src/utils/streamId/makeStreamId.ts
+++ src/utils/streamId/makeStreamId.ts
@@ -1,4 +1,4 @@
-export type StreamId = `${string}-${string}`
+import type { StreamId } from '@utils/streamId/StreamId.js'
 
 export function makeStreamId<Stream extends string, Id extends string>(stream: Stream, id: Id): StreamId {
   return `${stream}-${id}`
