f4870550c7ce635da8912628cb932349c4427dc2
jvhellemondt <j62676@eon.com>
2025-07-15 09:24:24 +0200

    refactor: update event store to use StreamKey type

--- src/infrastructure/EventStore/EventStore.ts
+++ src/infrastructure/EventStore/EventStore.ts
@@ -1,9 +1,10 @@
 import type { DomainEvent } from '@domain/DomainEvent.js'
 import type { OutboxEntry } from '@infrastructure/EventStore/OutboxEntry.js'
+import type { StreamKey } from '@utils/streamKey/index.js'
 
 export interface EventStore {
-  append: <TEvent extends DomainEvent<TEvent['payload']>>(streamId: string, events: TEvent[]) => Promise<void>
-  load: <TEvent extends DomainEvent<TEvent['payload']>>(streamId: string) => Promise<TEvent[]>
+  append: <TEvent extends DomainEvent<TEvent['payload']>>(streamKey: StreamKey, events: TEvent[]) => Promise<void>
+  load: <TEvent extends DomainEvent<TEvent['payload']>>(streamKey: StreamKey) => Promise<TEvent[]>
   getOutboxBatch: (limit?: number) => OutboxEntry[]
   acknowledgeDispatch: (id: string) => void
 }
