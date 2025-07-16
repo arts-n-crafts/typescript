1500000a516f0f6d657fd0c9acc4b392f744b0ce
Jens van Hellemondt <jens@invictus.codes>
2025-07-16 21:12:53 +0200

    chore: exports specification pattern

---
+++ src/domain/Specification/index.ts
@@ -0,0 +1,5 @@
+export * from './implementations/FieldEquals.specification.ts'
+export * from './implementations/FieldGreaterThan.specification.ts'
+export * from './QueryNode.ts'
+export * from './Specification.ts'
+export * from './utils/createQueryNode.ts'
