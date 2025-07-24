456199d93d52e3e041c1d64116d0954ed6e0d521
Jens van Hellemondt <>
2025-07-24 11:50:40 +0200

    refactor(Decider): remove unnecessary typing

--- src/domain/Decider.ts
+++ src/domain/Decider.ts
@@ -1,6 +1,6 @@
 import type { DomainEvent } from '@domain/DomainEvent.ts'
 
-export interface Decider<TState, TCommand, TEvent extends DomainEvent<TEvent['payload']>> {
+export interface Decider<TState, TCommand, TEvent extends DomainEvent> {
   decide(this: void, command: TCommand, currentState: TState): TEvent[]
   evolve(this: void, currentState: TState, event: TEvent): TState
   initialState(this: void, id: string): TState
