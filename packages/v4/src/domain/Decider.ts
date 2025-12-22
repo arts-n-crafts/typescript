4beef9f8a0eda094cf68aeaebb900d6bec417c31
Jens van Hellemondt <>
2025-12-22 12:49:57 +0100

    chore: enable Decider to have optional TRejection

--- packages/v4/src/domain/Decider.ts
+++ packages/v4/src/domain/Decider.ts
@@ -1,7 +1,7 @@
 import type { DomainEvent } from '@domain/DomainEvent.ts'
 import type { Rejection } from '@domain/Rejection.ts'
 
-export interface Decider<TState, TCommand, TEvent extends DomainEvent, TRejection extends Rejection> {
+export interface Decider<TState, TCommand, TEvent extends DomainEvent, TRejection extends Rejection = never> {
   decide(this: void, command: TCommand, currentState: TState): (TEvent | TRejection)[]
   evolve(this: void, currentState: TState, event: TEvent): TState
   initialState(this: void, id: string): TState
