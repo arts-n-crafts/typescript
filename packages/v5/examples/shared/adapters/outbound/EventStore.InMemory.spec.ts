2f52839bdf29e8ac678fb96ddffd611c26e34d71
Jens van Hellemondt <jens@invictus.codes>
2026-04-22 01:20:21 +0200

    feat: add AppendsDomainEvents to InMemoryEventStore example

--- packages/v5/examples/shared/adapters/outbound/EventStore.InMemory.spec.ts
+++ packages/v5/examples/shared/adapters/outbound/EventStore.InMemory.spec.ts
@@ -2,13 +2,14 @@ import type { LoadsDomainEvents } from "@adapters/outbound/LoadsDomainEvents.ts"
 import type { DomainEvent } from "@core/shapes/DomainEvent.ts"
 import { InMemoryEventStore } from "./EventStore.InMemory.ts"
 import { randomUUID } from "node:crypto"
+import type { AppendsDomainEvents } from "@adapters/outbound/AppendsDomainEvents.ts"
 
 interface TestDomainEvent extends DomainEvent<'TestDomainEvent', { name: string }> {}
 
 describe('in-memory event store', () => {
   const aggregateId = randomUUID()
   const streamName = 'users'
-  let eventStore: LoadsDomainEvents<TestDomainEvent, TestDomainEvent[]>
+  let eventStore: LoadsDomainEvents<Promise<TestDomainEvent[]>> & AppendsDomainEvents<TestDomainEvent>
 
   beforeEach(() => {
     eventStore = new InMemoryEventStore()
@@ -18,8 +19,13 @@ describe('in-memory event store', () => {
     expect(InMemoryEventStore).toBeDefined()
   })
 
-  it('should load domain events', () => {
-    const events = eventStore.load(streamName, aggregateId)
+  it('should load domain events', async () => {
+    const events = await eventStore.load(streamName, aggregateId)
     expect(events).toEqual([])
+  })
+
+  it('should append domain events', async () => {
+    const events = await eventStore.append(aggregateId, [])
+    expect(events).toEqual(undefined)
   })
 })
--- packages/v5/examples/shared/adapters/outbound/EventStore.InMemory.ts
+++ packages/v5/examples/shared/adapters/outbound/EventStore.InMemory.ts
@@ -1,8 +1,14 @@
+import type { AppendsDomainEvents } from "@adapters/outbound/AppendsDomainEvents.ts";
 import { type LoadsDomainEvents } from "@adapters/outbound/LoadsDomainEvents.ts";
+import type { DomainEvent } from "@core/shapes/DomainEvent.ts";
 
 export class InMemoryEventStore<TEvent>
-  implements LoadsDomainEvents<TEvent, TEvent[]> {
-  load(streamName: string, aggregateId: string): TEvent[] {
+  implements LoadsDomainEvents<Promise<TEvent[]>>, AppendsDomainEvents<DomainEvent> {
+  async load(streamName: string, aggregateId: string): Promise<TEvent[]> {
     return []
+  }
+
+  async append(aggregateId: string, domainEvents: DomainEvent[]): Promise<void> {
+    return
   }
 }
